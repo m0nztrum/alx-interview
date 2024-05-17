f409644311634e88524edd56e4adf1625fc86ef4
Dragonixj <delafiado@gmail.com>
2024-05-17 13:51:57 +0000

    done and tested

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -8,13 +8,14 @@ def init():
     """Validation point
 
     Returns:
-        N(int)
+        N(int): Number of queens
     """
     # checks for the correct num of command-line args
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-    if sys.argv[1].isdigit:
+    # Validates if N is an integer
+    if sys.argv[1].isdigit():
         N = int(sys.argv[1])
     else:
         print("N must be a number")
@@ -26,7 +27,30 @@ def init():
     return N
 
 
-def formatted_solution(positions):
+def is_valid(positions, row, col):
+    """Checks to see if the queen can be placed at row or col"""
+    for i in range(row):
+        if (
+            positions[i] == col
+            or positions[i] - i == col - row
+            or positions[i] + i == col + row
+        ):
+            return False
+    return True
+
+
+def solve_nqueens(size, row, positions, solutions):
+    """Recursively puts the queens on the board and sotres the solution"""
+    if row == size:
+        solutions.append(positions[:])
+    else:
+        for col in range(size):
+            if is_valid(positions, row, col):
+                positions[row] = col
+                solve_nqueens(size, row + 1, positions, solutions)
+
+
+def solution_format(positions):
     """Formats the solution for printing"""
     formatted_solution = [[row, col] for row, col in enumerate(positions)]
     return formatted_solution
@@ -34,6 +58,15 @@ def formatted_solution(positions):
 
 def nqueens():
     N = init()
+
+    # Finding all solutions
+    solutions = []
+    positions = [-1] * N
+    solve_nqueens(N, 0, positions, solutions)
+
+    # print out a formatted output of all solutions
+    for solution in solutions:
+        print(solution_format(solution))
 
 
 if __name__ == "__main__":
