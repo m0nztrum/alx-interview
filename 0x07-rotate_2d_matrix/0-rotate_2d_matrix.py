87db02e7ebdcf9542833a9fa4b6be430c888aa21
Dragonixj <delafiado@gmail.com>
2024-06-02 21:16:11 +0000

    task done

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def rotate_2d_matrix(matrix):
+    """rotates 2d matrix 90 clockwise"""
+    n = len(matrix)
+
+    for i in range(n):
+        for j in range(i):
+            # Swap elements at position(i, j) with elements at position(j, i)
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    # reverse rows
+    for i in range(n):
+        matrix[i].reverse()
