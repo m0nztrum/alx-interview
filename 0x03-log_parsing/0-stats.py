afd05d0362deb4dffe1b777b5666fcabef593a4f
m0nztrum <delafiado@gmail.com>
2024-07-04 14:52:40 +0000

    corrections

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,58 +1,42 @@
 #!/usr/bin/python3
-"""Module reads from standard input and computes metrics"""
+"""Script that reads stdin line by line and computes metrics"""
 
+import re
+import signal
 import sys
 
+total_size = 0
 
-def process_input(line, cache, total_size, counter):
-    """Process input line and update cache and total size"""
-    line_list = line.split(" ")
-    if len(line_list) > 4:
-        code = line_list[-2]
-        size = int(line_list[-1])
-        if code in cache:
-            cache[code] += 1
-        total_size += size
-        counter += 1
-    return total_size, counter
+status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+
+# pattern for the log line
+pattern = (
+    r'(\d+\.\d+\.\d+\.\d+) - \[(.+)\] "GET /projects/260 HTTP/1.1" ' r"(\d+) (\d+)"
+)
 
 
-def print_statistics(cache, total_size):
-    """Print statistics"""
-    print("File size:", total_size)
-    for key, value in sorted(cache.items()):
-        if value != 0:
-            print("{}: {}".format(key, value))
+def print_metrics(signum=None, frame=None):
+    """Function to print metrics"""
+    print(f"File size: {total_size}")
+    for code in sorted(status_codes.keys()):
+        if status_codes[code] > 0:
+            print(f"{code}: {status_codes[code]}")
 
 
-def main():
-    """Main function to read from standard input and compute metrics"""
-    cache = {
-        "200": 0,
-        "301": 0,
-        "400": 0,
-        "401": 0,
-        "403": 0,
-        "404": 0,
-        "405": 0,
-        "500": 0,
-    }
-    total_size = 0
-    counter = 0
-
-    try:
-        for line in sys.stdin:
-            total_size, counter = process_input(line, cache, total_size, counter)
-            if counter == 10:
-                counter = 0
-                print_statistics(cache, total_size)
-
-    except KeyboardInterrupt:
-        pass
-
-    finally:
-        print_statistics(cache, total_size)
+signal.signal(signal.SIGINT, print_metrics)
 
 
-if __name__ == "__main__":
-    main()
+for i, line in enumerate(sys.stdin, start=1):
+    match = re.match(pattern, line)
+    if match:
+        total_size += int(match.group(4))  # File size
+        status_code = int(match.group(3))  # Status code
+        if status_code in status_codes:
+            status_codes[status_code] += 1
+        else:
+            pass
+
+    if i != 0 and i % 10 == 0:
+        print_metrics()
+
+print_metrics()
